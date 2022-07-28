a2b6f1197cf6fadaa3b57c57135916e94432223a
Shohei <Shohei0723.1999@gmail.com>
2022-07-28 17:33:08 +0900

    Update UDPReceive

--- Unity_Script/UDPReceive.cs
+++ Unity_Script/UDPReceive.cs
@@ -4,6 +4,8 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
+using OpenCVForUnity;
 
 public class UDPReceive : MonoBehaviour
 {
@@ -14,15 +16,27 @@ public class UDPReceive : MonoBehaviour
 
     public static Action<string> DataCallBack;
 
+    private int width, height, near_x, near_y, val, rect_tl_x, rect_tl_y, rect_br_x, rect_br_y;
+    private int nothing;
+
     void Start()
     {
         UDPReceive.DataCallBack += Method;
         UDPStart();
+        width = 640;
+        height = 480;
+        near_x = 0;
+        near_y = 0;
+        val = 100;
+        rect_tl_x = 0;
+        rect_tl_y = 0;
+        rect_br_x = 0;
+        rect_br_y = 0;
+        nothing = 0;
     }
 
     public void UDPStart()
     {
-
         udp = new UdpClient(LOCAL_PORT);
         thread = new Thread(new ThreadStart(ThreadMethod));
 
@@ -37,19 +51,53 @@ public class UDPReceive : MonoBehaviour
     void OnApplicationQuit()
     {
         thread.Abort();
+        udp.Close();
     }
 
-    private static void ThreadMethod()
+    private void ThreadMethod()
     {
         while (true)
         {
-
             IPEndPoint remoteEP = null;
             byte[] data = udp.Receive(ref remoteEP);
-            string text = Encoding.ASCII.GetString(data);
+            string s = Encoding.ASCII.GetString(data);
+            string[] arr = s.Split(',');
 
-            DataCallBack(text);
-            Debug.Log(text);
+            width = Int32.Parse(arr[1]);
+            height = Int32.Parse(arr[0]);
+            near_x = Int32.Parse(arr[2]);
+            near_y = Int32.Parse(arr[3]);
+            val = Int32.Parse(arr[4]);
+            rect_tl_x = Int32.Parse(arr[5]);
+            rect_tl_y = Int32.Parse(arr[6]);
+            rect_br_x = Int32.Parse(arr[7]);
+            rect_br_y = Int32.Parse(arr[8]);
+            nothing = Int32.Parse(arr[9]);
         }
+    }
+
+    public Vector2Int getFrameSize()
+    {
+        return new Vector2Int(width, height);
+    }
+
+    public Vector3Int getNearPoint()
+    {
+        return new Vector3Int(near_x, near_y, val);
+    }
+
+    public Vector4 getRect()
+    {
+        return new Vector4(rect_tl_x, rect_tl_y, rect_br_x, rect_br_y);
+    }
+    public int get_is_Nothing()
+    {
+        return nothing;
+    }
+
+    public void SceneChange()
+    {
+        thread.Abort();
+        udp.Close();
     }
 }
